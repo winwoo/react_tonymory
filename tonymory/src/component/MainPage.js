e85ff0a85461a386e281a5c343596989a6158581
DESKTOP-QAU9GUE\winwoo <winwoo4946@gmail.com>
2022-01-09 21:21:33 +0900

    이미지 폴더 추가

--- tonymory/src/component/MainPage.js
+++ tonymory/src/component/MainPage.js
@@ -1,12 +1,21 @@
 import "../css/common.css";
 import "../css/mainPage.css";
 
-function MainPage(){
-    return(
+
+// import 이미지 변수명 from getImg(이미지 파일명);
+
+function MainPage() {
+    return (
         <>
             {/* 여기에 Dom 작성 */}
         </>
     );
+}
+
+
+function getImg(imgName) {
+    const IMG_PATH = "../img/";
+    return IMG_PATH + imgName;
 }
 
 export default MainPage;
