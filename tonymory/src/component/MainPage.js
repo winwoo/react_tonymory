9afe4689838f5266f5cc7385c0ff6e16e6cc5154
hnlim <hnlim2244@gmail.com>
2022-01-09 21:58:47 +0900

    header 작업

--- tonymory/src/component/MainPage.js
+++ tonymory/src/component/MainPage.js
@@ -4,21 +4,37 @@ import "../css/mainPage.css";
 
 
 // import 이미지 변수명 from getImg(이미지 파일명);
+import tonyLogo from "../img/tonyLogo.png";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 function MainPage() {
     return (
         <>
-            <FontAwesomeIcon icon={faSearch} className="search" />
-            {/* 여기에 Dom 작성 */}
+            <div className="Wrap">
+                <header className="header">
+                    <div>
+                        <img src={tonyLogo}></img>
+                    </div>
+                    <div className="searchCon">
+                        <input type={text}></input>
+                    </div>
+                    <div className="userCon">
+                        <div className="userList">
+                            <span>로그인</span>
+                            <span>회원가입</span>
+                            <span>고객센터</span>
+                        </div>
+                        <ul className="iconBox">
+                            <li>
+                                test
+                            </li>
+                        </ul>
+                    </div>
+                </header>
+            </div>
         </>
     );
 }
 
-
-function getImg(imgName) {
-    const IMG_PATH = "../img/";
-    return IMG_PATH + imgName;
-}
 
 export default MainPage;
