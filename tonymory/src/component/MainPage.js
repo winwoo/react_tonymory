8e777aee70bc7e4d4a8e138c42bd1e6fe9336ae7
DESKTOP-8GCOB95\hahaha <hnlim2244@gmail.com>
2022-02-09 16:32:07 +0900

    nav 작업

--- tonymory/src/component/MainPage.js
+++ tonymory/src/component/MainPage.js
@@ -54,8 +54,8 @@ function MainPage() {
             </div> {/*End::contentWrap*/}
 
             {/*start::navContainer*/}
-            <div className="navContainer">
-                <div className="contentWrap">
+            <div className="contentWrap">
+                <div className="navContainer">
                     <div className="navInner">
                         <div className="btnMenu">
                             <A>
@@ -68,10 +68,115 @@ function MainPage() {
                             <A>신상</A>
                             <A>커뮤니티</A>
                             <A>토니팁톡</A>
-                            <A>소킹TV</A>
+                            <A>쇼킹TV</A>
                             <A>기획전</A>
                             <A>이벤트</A>
                         </nav>
+
+                        {/* nav 마우스오버 했을 때*/}
+                        <div className="navCategoryCon">
+                            
+                            <div className="categoryList">
+                                <ul className="categoryInner">
+                                    <li class="navMainName">
+                                        <A>기초</A>
+                                    </li>
+                                    <ul className="depth2">
+                                        <li><A>스킨케어</A></li>
+                                        <li><A>클렌징</A></li>
+                                        <li><A>스페셜 케어</A></li>
+                                    </ul>
+                                </ul>
+                                <ul className="categoryInner">
+                                    <li className="navMainName">
+                                        <A>메이크업</A>
+                                    </li>
+                                    <ul className="depth2">
+                                        <li><A>페이스</A></li>
+                                        <li><A>립</A></li>
+                                        <li><A>아이</A></li>
+                                        <li><A>네일</A></li>
+                                    </ul>
+                                </ul>
+                                <ul className="categoryInner">
+                                    <li className="navMainName">
+                                        <A>바디/헤어</A>
+                                    </li>
+                                    <ul className="depth2">
+                                        <li><A>바디</A></li>
+                                        <li><A>헤어</A></li>
+                                    </ul>
+                                </ul>
+                                <ul className="categoryInner">
+                                    <li className="navMainName">
+                                        <A>향수/디퓨저/캔들</A>
+                                    </li>
+                                    <ul className="depth2">
+                                        <li><A>향수</A></li>
+                                        <li><A>패브릭퍼퓸</A></li>
+                                        <li><A>캔들/디퓨저</A></li>
+                                    </ul>
+                                </ul>
+                                <ul className="categoryInner">
+                                    <li className="navMainName">
+                                        <A>미용소품</A>
+                                    </li>
+                                    <ul className="depth2">
+                                        <li><A>스킨케어소품</A></li>
+                                        <li><A>메이크업소품</A></li>
+                                        <li><A>헤어소품</A></li>
+                                        <li><A>바디소품</A></li>
+                                        <li><A>네일소품</A></li>
+                                        <li><A>생활소품</A></li>
+                                        <li><A>기타소품</A></li>
+                                    </ul>
+                                </ul>
+                                <ul className="categoryInner">
+                                    <li className="navMainName">
+                                        <A>디지털/가전</A>
+                                    </li>
+                                    <ul className="depth2">
+                                        <li><A>이미용가전</A></li>
+                                        <li><A>생활가전</A></li>
+                                    </ul>
+                                </ul>
+                                <ul className="categoryInner">
+                                    <li className="navMainName">
+                                        <A>라이프</A>
+                                    </li>
+                                    <ul className="depth2">
+                                        <li><A>푸드</A></li>
+                                        <li><A>리빙</A></li>
+                                        <li><A>반려동물</A></li>
+                                    </ul>
+                                </ul>
+                                <ul className="categoryInner">
+                                    <li className="navMainName">
+                                        <A>옴므</A>
+                                    </li>
+                                    <ul className="depth2">
+                                        <li><A>스킨케어</A></li>
+                                        <li><A>메이크업</A></li>
+                                        <li><A>클렌징케어</A></li>
+                                        <li><A>기타</A></li>
+                                    </ul>
+                                </ul>
+                                <ul className="categoryInner">
+                                    <li className="navMainName">
+                                        <A>온라인전용</A>
+                                    </li>
+                                    <ul className="depth2">
+                                        <li><A>기초</A></li>
+                                        <li><A>메이크업</A></li>
+                                        <li><A>바디/헤어</A></li>
+                                        <li><A>옴므</A></li>
+                                        <li><A>미용소품</A></li>
+                                    </ul>
+                                </ul>
+                            </div>
+                        </div>
+
+
                     </div>
                 </div>
             </div>
